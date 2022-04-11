84da79703844a545202f9bdbcea6542bfc7b4e19
menasehk <menasehk38@gmail.com>
2022-04-11 08:36:41 +0300

    image place holder updateed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React,{useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login'
-import Register from './components/RegisterDriver'
+import Login from './pages/LoginPage'
+import Register from './pages/RegisterPage'
 import {Route,Routes} from 'react-router-dom'
+import { getFromSession} from "./utills.js";
 function App() {
+  function verifyLoggedIn() {
+    const token = getFromSession("auth-token");
+    return token ? true : false;
+  }
+
+  useEffect(() => {
+    const loggedIn = verifyLoggedIn();
+    console.log(loggedIn, "--------------");
+    if (!loggedIn && window.location.pathname !=="/login") {
+      window.history.replaceState({}, undefined, "/login");
+      window.location.reload();
+    } else {
+      if (window.location.pathname === "/login" && loggedIn) {
+        window.history.back();
+        window.location.reload();
+      }
+    }
+  }, [])
   return (
     <div className="App">
      <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/" element={<Register/>} />
      </Routes>
     </div>
   );
